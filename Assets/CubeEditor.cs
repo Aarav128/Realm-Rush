06491f8adebbe87344c3e7bcea4d7e1ce7718b46
AaravInAction <30843771+AaravInAction@users.noreply.github.com>
2019-07-17 22:08:01 +0530

    07-17-2019 Cube has position label

---
+++ Assets/CubeEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+    [Range(1f, 20f)] [SerializeField] private float gridSize = 10f;
+
+    private TextMesh textMesh;
+
+    private void Update() 
+    {
+        Vector3 snapPos;
+        
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x / gridSize + ", " + snapPos.z / gridSize;
+    }
+}

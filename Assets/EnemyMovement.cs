60ee318ff5f6b37ba1a2850efa08260be78f0e80
AaravInAction <30843771+AaravInAction@users.noreply.github.com>
2019-07-18 11:51:12 +0530

    07-18-2019 Enemy follows a set of waypoints using Coroutines.

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour 
+{
+    [SerializeField] List<Waypoint> path = null;
+
+    private void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    private IEnumerator FollowPath()
+    {
+        print("Starting patrol...");
+        foreach (Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            print("Visiting: " + waypoint);
+            yield return new WaitForSeconds(1f);
+        }
+        print("Ending patrol");
+    }
+}
